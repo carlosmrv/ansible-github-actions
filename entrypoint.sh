321179474d7cee43d336850e7583d24d507e8a9d
X412151 <c.rodriguezvergel@servexternos.gruposantander.com>
2021-10-03 16:54:52 +0200

    fix typo :bug:

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "${DAY_OF_WEEK}"
 echo "${USERNAME}"
-echo "${github.event.inputs.PROPERTIES}"
-echo "${github.event.inputs.tags}"
+echo "${PROPERTIES}"
+echo "${tags}"
 python3 --version
 ansible 127.0.0.1 -m ping --connection=local
